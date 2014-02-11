70cd459bd96d81714d2ea6db95415f2281eac6d7
Kamen Hursev <kamen.hursev@gmail.com>
2014-02-11 09:23:42 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MediaBlog::Application.config.secret_key_base = '3bd1047a732e83e9ef03eede60a5f18e55df6cebb8efc3b9e30f9178e939c61a56f69924792aa98e1d7f410d40d18b6fb9697c88f024abb0988bd3dd1f76ac38'
