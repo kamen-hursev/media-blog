e9aaa822026106586cda11108d92e5c8b8eb9297
Kamen Hursev <kamen.hursev@gmail.com>
2014-02-16 19:09:29 +0200

    add some styles

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -28,7 +28,7 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+        format.html { redirect_to :reports, notice: 'Report was successfully created.' }
         format.json { render action: 'show', status: :created, location: @report }
       else
         format.html { render action: 'new' }
