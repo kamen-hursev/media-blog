b1d26852f96bc000ea70d3a92570611b00109c0c
Kamen Hursev <kamen.hursev@gmail.com>
2014-02-23 01:30:13 +0200

    add report tests

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save invalid report' do
+    report = Report.new
+    assert report.invalid?
+    assert report.errors[:title].any?, 'report without a title is valid'
+    assert report.errors[:slug].any?, 'report without a slug is valid'
+    assert report.errors[:category].any?, 'report without a category is valid'
+    assert report.errors[:body].any?, 'report without a body is valid'
+  end
+
+  test 'should not save two reports with the same slug' do
+    report = Report.new(slug: reports(:sochi).slug)
+    assert report.invalid?
+    assert report.errors[:slug].any?, 'report with existing slug is valid'
+  end
+
+  test 'should save report with valid fields' do
+    report = Report.new(
+      title: 'test title',
+      slug: 'slug_written',
+      body: 'some body text goes here',
+      category: categories(:sport)
+    )
+    assert report.valid?
+  end
 end
