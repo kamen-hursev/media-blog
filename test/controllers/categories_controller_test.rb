92714831316c128d9b416057bcddaf8d7a902b99
Kamen Hursev <kamen.hursev@gmail.com>
2014-02-23 02:12:23 +0200

    improve tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,18 +5,36 @@ class CategoriesControllerTest < ActionController::TestCase
     @category = categories(:sport)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:categories)
   end
 
-  test "should get new" do
+  test 'should have table with results' do
+    get :index
+    assert_response :success
+    assert_select 'table tbody' do |elements|
+      elements.each do |element|
+        assert_select element, 'tr'
+      end
+    end
+  end
+
+  test 'categories with reports should not have delete button' do
+    get :index
+    Category.all.each do |category|
+      assert_select "table tr[data-id=#{category.id}] .btn-destroy",
+                    category.reports.size > 0 ? 0 : 1
+    end
+  end
+
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create category" do
+  test 'should create category' do
     assert_difference('Category.count') do
       post :create, category: { name: 'Test category' }
     end
@@ -24,22 +42,22 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_redirected_to :categories
   end
 
-  test "should show category" do
+  test 'should show category' do
     get :show, id: @category
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @category
     assert_response :success
   end
 
-  test "should update category" do
+  test 'should update category' do
     patch :update, id: @category, category: { name: @category.name }
     assert_redirected_to category_path(assigns(:category))
   end
 
-  test "should destroy category" do
+  test 'should destroy category' do
     category = categories(:fun)
     assert_difference('Category.count', -1) do
       delete :destroy, id: category
--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -5,41 +5,47 @@ class ReportsControllerTest < ActionController::TestCase
     @report = reports(:president)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:reports)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create report" do
+  test 'should create report' do
     assert_difference('Report.count') do
-      post :create, report: { body: 'test body', slug: 'some_report_title', title: 'Some Report Title', category_id: categories(:sport).id }
+      post :create, report: { body: 'test body', slug: 'some_report_title',
+                              title: 'Some Report Title',
+                              category_id: categories(:sport).id }
     end
 
     assert_redirected_to :reports
   end
 
-  test "should show report" do
+  test 'should show report' do
     get :show, id: @report
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @report
     assert_response :success
   end
 
-  test "should update report" do
-    patch :update, id: @report, report: { body: @report.body, slug: @report.slug, title: @report.title }
+  test 'should update report' do
+    patch :update, id: @report, report: {
+      body: @report.body,
+      slug: @report.slug,
+      title: @report.title
+    }
     assert_redirected_to report_path(assigns(:report))
   end
 
-  test "should destroy report" do
+  test 'should destroy report' do
     assert_difference('Report.count', -1) do
       delete :destroy, id: @report
     end
