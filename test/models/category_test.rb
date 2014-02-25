908a41a8c92b270553d2a825defe6bf333ef06fa
Kamen Hursev <kamen.hursev@gmail.com>
2014-02-25 10:12:33 +0200

    fix test names

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  test 'should not save category without name' do
+  test 'should not allow category without name' do
     category = Category.new
     assert category.invalid?, 'category without a name is valid'
     assert category.errors[:name].any?
   end
 
-  test 'should not save two categories with the same name' do
+  test 'should not allow two categories with the same name' do
     category = Category.new(name: categories(:sport).name)
     assert category.invalid?, 'category with existing name is valid'
     assert category.errors[:name].any?
@@ -19,5 +19,4 @@ class CategoryTest < ActiveSupport::TestCase
       assert !category.destroy, 'category with reports in it is destroyed'
     end
   end
-
 end
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test 'should not save invalid report' do
+  test 'should not allow invalid report' do
     report = Report.new
     assert report.invalid?
     assert report.errors[:title].any?, 'report without a title is valid'
@@ -10,13 +10,13 @@ class ReportTest < ActiveSupport::TestCase
     assert report.errors[:body].any?, 'report without a body is valid'
   end
 
-  test 'should not save two reports with the same slug' do
+  test 'should not allow two reports with the same slug' do
     report = Report.new(slug: reports(:sochi).slug)
     assert report.invalid?
     assert report.errors[:slug].any?, 'report with existing slug is valid'
   end
 
-  test 'should save report with valid fields' do
+  test 'should allow report with valid fields' do
     report = Report.new(
       title: 'test title',
       slug: 'slug_written',
